5fe67b50334df80ebb0e3b68b7a0503dfce8e715
Luci Lua <luci.lua81@gmail.com>
2021-12-22 15:19:29 -0300

    🖱 Scroll: remove btn, left bar more clean

--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -10,120 +10,86 @@ function Sidebar() {
                 <ul className={classNames(styles.menus, styles.boxWhite)}>
                     <li><p>Grupos</p>
                         <ul className={classNames(styles.menu, styles.menuGrupos)}>
-                            <li className={classNames(styles.menuItem)}>
-                                <UserGroupIcon className={styles.icoItemMenu} />
-                                <Link href="/osu-group">
+                            <Link href="/osu-group">
                                 <a>
-                                    !osu group!
+                                    <li className={classNames(styles.menuItem)}>
+                                        <UserGroupIcon className={styles.icoItemMenu} />
+                                        !osu group dos melhorres do mudno!
+                                    </li>
                                 </a>
-                                </Link>
-                            </li>
-                            <li className={classNames(styles.menuItem)}>
-                                <UserGroupIcon className={styles.icoItemMenu} />
-                                <a href='./'>
-                                    Nostalgia
+                            </Link>
+                            <Link href="/osu-group">
+                                <a>
+                                    <li className={classNames(styles.menuItem)}>
+                                        <UserGroupIcon className={styles.icoItemMenu} />
+                                        !osu group!
+                                    </li>
                                 </a>
-                            </li>
-                            <li className={classNames(styles.menuItem)}>
-                                <UserGroupIcon className={styles.icoItemMenu} />
-                                <a href='./'>
-                                    Amo passas
+                            </Link>
+                            <Link href="/osu-group">
+                                <a>
+                                    <li className={classNames(styles.menuItem)}>
+                                        <UserGroupIcon className={styles.icoItemMenu} />
+                                        !osu group!
+                                    </li>
                                 </a>
-                            </li>
+                            </Link>
                         </ul>
                     </li>
                     <li><p>Playlists</p>
                         <ul className={classNames(styles.menu, styles.menuPlaylists)}>
-                            <li className={classNames(styles.menuItem)}>
-                                <MusicNoteIcon className={styles.icoItemMenu} />
-                                <a href='./'>
-                                    Adele
+                            <Link href="/osu-group">
+                                <a>
+                                    <li className={classNames(styles.menuItem)}>
+                                        <MusicNoteIcon className={styles.icoItemMenu} />
+                                        !osu group!
+                                    </li>
                                 </a>
-                            </li>
-                            <li className={classNames(styles.menuItem)}>
-                                <MusicNoteIcon className={styles.icoItemMenu} />
-                                <a href='./'>
-                                    Aurora
+                            </Link>
+                            <Link href="/osu-group">
+                                <a>
+                                    <li className={classNames(styles.menuItem)}>
+                                        <MusicNoteIcon className={styles.icoItemMenu} />
+                                        !osu group!
+                                    </li>
                                 </a>
-                            </li>
-                            <li className={classNames(styles.menuItem)}>
-                                <MusicNoteIcon className={styles.icoItemMenu} />
-                                <a href='./'>
-                                    Sertanejo
+                            </Link>
+                            <Link href="/osu-group">
+                                <a>
+                                    <li className={classNames(styles.menuItem)}>
+                                        <MusicNoteIcon className={styles.icoItemMenu} />
+                                        !osu group!
+                                    </li>
                                 </a>
-                            </li>
-                            <li className={classNames(styles.menuItem)}>
-                                <MusicNoteIcon className={styles.icoItemMenu} />
-                                <a href='./'>
-                                    POP
+                            </Link>                            <Link href="/osu-group">
+                                <a>
+                                    <li className={classNames(styles.menuItem)}>
+                                        <MusicNoteIcon className={styles.icoItemMenu} />
+                                        !osu group!
+                                    </li>
                                 </a>
-                            </li>
-                            <li className={classNames(styles.menuItem)}>
-                                <MusicNoteIcon className={styles.icoItemMenu} />
-                                <a href='./'>
-                                    Lady Gaga
+                            </Link>                            <Link href="/osu-group">
+                                <a>
+                                    <li className={classNames(styles.menuItem)}>
+                                        <MusicNoteIcon className={styles.icoItemMenu} />
+                                        !osu group!
+                                    </li>
                                 </a>
-                            </li>
-                            <li className={classNames(styles.menuItem)}>
-                                <MusicNoteIcon className={styles.icoItemMenu} />
-                                <a href='./'>
-                                    Engenheiros do Hawaii
+                            </Link>                            <Link href="/osu-group">
+                                <a>
+                                    <li className={classNames(styles.menuItem)}>
+                                        <MusicNoteIcon className={styles.icoItemMenu} />
+                                        !osu group!
+                                    </li>
                                 </a>
-                            </li>
-                            <li className={classNames(styles.menuItem)}>
-                                <MusicNoteIcon className={styles.icoItemMenu} />
-                                <a href='./'>
-                                    Funk
+                            </Link>                            <Link href="/osu-group">
+                                <a>
+                                    <li className={classNames(styles.menuItem)}>
+                                        <MusicNoteIcon className={styles.icoItemMenu} />
+                                        !osu group!
+                                    </li>
                                 </a>
-                            </li>
-                            <li className={classNames(styles.menuItem)}>
-                                <MusicNoteIcon className={styles.icoItemMenu} />
-                                <a href='./'>
-                                    POP
-                                </a>
-                            </li>
-                            <li className={classNames(styles.menuItem)}>
-                                <MusicNoteIcon className={styles.icoItemMenu} />
-                                <a href='./'>
-                                    Lady Gaga
-                                </a>
-                            </li>
-                            <li className={classNames(styles.menuItem)}>
-                                <MusicNoteIcon className={styles.icoItemMenu} />
-                                <a href='./'>
-                                    Engenheiros do Hawaii
-                                </a>
-                            </li>
-                            <li className={classNames(styles.menuItem)}>
-                                <MusicNoteIcon className={styles.icoItemMenu} />
-                                <a href='./'>
-                                    Funk
-                                </a>
-                            </li>
-                            <li className={classNames(styles.menuItem)}>
-                                <MusicNoteIcon className={styles.icoItemMenu} />
-                                <a href='./'>
-                                    POP
-                                </a>
-                            </li>
-                            <li className={classNames(styles.menuItem)}>
-                                <MusicNoteIcon className={styles.icoItemMenu} />
-                                <a href='./'>
-                                    Lady Gaga
-                                </a>
-                            </li>
-                            <li className={classNames(styles.menuItem)}>
-                                <MusicNoteIcon className={styles.icoItemMenu} />
-                                <a href='./'>
-                                    Engenheiros do Hawaii
-                                </a>
-                            </li>
-                            <li className={classNames(styles.menuItem)}>
-                                <MusicNoteIcon className={styles.icoItemMenu} />
-                                <a href='./'>
-                                    Funk
-                                </a>
-                            </li>
+                            </Link>
                         </ul>
                     </li>
                 </ul>
