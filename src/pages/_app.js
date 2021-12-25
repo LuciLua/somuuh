d26738c78804c589fb865e33a257596b339870fd
Luci Lua <luci.lua81@gmail.com>
2021-12-24 22:42:45 -0300

    :cactus: menu mobile modification

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -27,20 +27,39 @@ function App({ Component, pageProps }) {
       menus.style.width = '0'
       // encolhe estrutura de menus da barra esquerda
       barleft.style.width = '0'
+      barleft.style.maxWidth = '0'
+      barleft.style.minWidth = '0'
       setTimeout(() => {
         // esconde menu para evitar bug quanto a width
         menus.style.display = 'none'
       }, 250)
-      barcenter.style.width = '100%'
+      barcenter.style.padding = '5px'
+
+      barcenter.style.minWidth = '100vw'
+      barcenter.style.maxWidth = '100vw'
+      barcenter.style.width = '100vw'
+
+      barcenter.style.marginLeft = '0'
+
+
       containerSideBar.style.width = '0'
     }
     // aberto
     else {
+
       menus.style.display = 'flex'
       menus.style.width = '90px'
+
       barleft.style.width = '90px'
+      barleft.style.maxWidth = '90px'
+      barleft.style.minWidth = '90px'
+
+      barcenter.style.minWidth = 'calc(100vw - 95px)'
+      barcenter.style.maxWidth = 'calc(100vw - 95px)'
+      barcenter.style.width = 'calc(100vw - 95px)'
       
-      barcenter.style.width = 'calc(100vw - 105px)'
+      barcenter.style.marginLeft = 'auto'
+
       containerSideBar.style.width = '80px'
     }
 
