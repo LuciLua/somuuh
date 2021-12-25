d12777931b1d956051bd6cf3cd93d91430b854b4
Luci Lua <luci.lua81@gmail.com>
2021-12-24 22:26:42 -0300

    :cactus: menu only mobile, better now, sizes ok

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -7,16 +7,16 @@ import SocialBar from "../components/center/socialbar";
 
 function App({ Component, pageProps }) {
   function openCloseMenuLeft() {
-    const menus = document.querySelector('.menus')
 
-    const container = document.querySelector('.containerSidebar')
+    const menus = document.querySelector('.menus')
+    const containerSideBar = document.querySelector('.containerSidebar')
 
     menus.classList.toggle('open')
-    container.classList.toggle('open')
+    containerSideBar.classList.toggle('open')
 
     const barleft = document.querySelector('.barleft')
     const barcenter = document.querySelector('.containerCenter')
-    
+
     const centerAndRightBar = document.querySelector('.centerAndRightBar')
 
     const barright = document.querySelector('.barRight')
@@ -27,45 +27,21 @@ function App({ Component, pageProps }) {
       menus.style.width = '0'
       // encolhe estrutura de menus da barra esquerda
       barleft.style.width = '0'
-      setTimeout(()=> {
+      setTimeout(() => {
         // esconde menu para evitar bug quanto a width
         menus.style.display = 'none'
       }, 250)
-
-      barcenter.style.width = '100vw'
-      barcenter.style.minWidth = '100vw'
-      
-      barcenter.style.padding = '5px'
+      barcenter.style.width = '100%'
+      containerSideBar.style.width = '0'
     }
     // aberto
     else {
       menus.style.display = 'flex'
       menus.style.width = '90px'
-
-
-      menus.style.marginLeft = 'auto'
-
-      barleft.style.width= '250px'
-      container.style.width= '250px'
-
-      barleft.style.padding= '0'
-      barleft.style.margin= '0'
-
-      // container.style.padding= '0'
-      container.style.margin= '0'
-      // container.style.background = '#93d'
-
-      barcenter.style.minWidth = 'calc(100vw - 90px)'
-      barcenter.style.width = 'calc(100vw - 90px)'
+      barleft.style.width = '90px'
       
-      barcenter.style.marginLeft = 'auto'
-      barcenter.style.paddingLeft = '10px'
-
-      // container.style.width= '100px'
-
-      // barleft.style.background = '#000'
-
-      centerAndRightBar.style.width = 'fit-content'
+      barcenter.style.width = 'calc(100vw - 105px)'
+      containerSideBar.style.width = '80px'
     }
 
   }
