6fac04ce99610eca90224ee8f84e9922bb0553d7
Luci Lua <luci.lua81@gmail.com>
2023-11-23 20:05:49 -0300

    **refactor** replace <a> tag to <Link> next tag

--- src/app/(other_pages)/playlists/page.tsx
+++ src/app/(other_pages)/playlists/page.tsx
@@ -1,4 +1,5 @@
 import { Metadata } from "next"
+import Link from "next/link"
 import { AiOutlineGroup } from "react-icons/ai"
 
 export const metadata: Metadata = { title: "Playlists" }
@@ -10,32 +11,32 @@ export default function playlists({ params }) {
                 <h1 className="text-zinc-100 font-normal text-[30px] mb-[20px]">Meus grupos</h1>
                 <div className="flex flex-col  h-full">
 
-                    <a href="/playlists/maisouvidas" className=" py-[10px] px-[20px] gap-[10px] items-center flex w-full min-h-[30px]  p-[10px] hover:bg-zinc-700">
+                    <Link href="/playlists/maisouvidas" className=" py-[10px] px-[20px] gap-[10px] items-center flex w-full min-h-[30px]  p-[10px] hover:bg-zinc-700">
                     <div className="flex justify-center items-center rounded-full w-[60px] h-[60px] overflow-hidden relative">
                             <AiOutlineGroup />
                         </div>
                         <p>Playlist: Mais Ouvidas</p>
-                    </a>
+                    </Link>
                     <div className="w-full h-[1px] bg-zinc-700" />
-                    <a href="/playlists/favoritasdekpop" className=" py-[10px] px-[20px] gap-[10px] items-center flex w-full min-h-[30px]  p-[10px] hover:bg-zinc-700">
+                    <Link href="/playlists/favoritasdekpop" className=" py-[10px] px-[20px] gap-[10px] items-center flex w-full min-h-[30px]  p-[10px] hover:bg-zinc-700">
                         <div className="flex justify-center items-center rounded-full w-[60px] h-[60px] overflow-hidden relative">
                             <AiOutlineGroup />
                         </div>
                         <p>Playlist: Favoritas de kpop</p>
-                    </a>
+                    </Link>
                     <div className="w-full h-[1px] bg-zinc-700 " />
-                    <a href="/playlists/sertanejohits" className=" py-[10px] px-[20px] gap-[10px] items-center flex w-full min-h-[30px]  p-[10px] hover:bg-zinc-700">
+                    <Link href="/playlists/sertanejohits" className=" py-[10px] px-[20px] gap-[10px] items-center flex w-full min-h-[30px]  p-[10px] hover:bg-zinc-700">
                     <div className="flex justify-center items-center rounded-full w-[60px] h-[60px] overflow-hidden relative">
                             <AiOutlineGroup />
                         </div>
                         <p>Playlist: Sertanejo hits</p>
-                    </a>
-                    <a href="/playlists/maistocadasdopop" className=" py-[10px] px-[20px] gap-[10px] items-center flex w-full min-h-[30px]  p-[10px] hover:bg-zinc-700">
+                    </Link>
+                    <Link href="/playlists/maistocadasdopop" className=" py-[10px] px-[20px] gap-[10px] items-center flex w-full min-h-[30px]  p-[10px] hover:bg-zinc-700">
                     <div className="flex justify-center items-center rounded-full w-[60px] h-[60px] overflow-hidden relative">
                             <AiOutlineGroup />
                         </div>
                         <p>Playlist: Mais tocadas do POP</p>
-                    </a>
+                    </Link>
                 </div>
             </div>
         </div>
